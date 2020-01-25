649b25fac0bf928941aac352dd92898b3ede5624
unknown <dariansung@umail.ucsb.edu>
2020-01-24 17:40:22 -0800

    DS - Attempting next.js style Hello World

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,21 @@ let http = require('http');
 port = process.env.PORT || process.argv[2] || 8080;
 
 // Hello World server
+/*
 let server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('Hello World!');
     res.end();
 });
+*/
+
+const Index = () => (
+  <div>
+    <p>Hello World!</p>
+  </div>
+);
+
+export default Index;
 
 // Listen on port
 server.listen(port, function () {
