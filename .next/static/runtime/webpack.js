8bd8f2c1fec3d1ae2632fc0659fc7a318ac6e4b1
unknown <dariansung@umail.ucsb.edu>
2020-01-24 17:50:52 -0800

    DS - Added to scripts in package.json

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bb84a4d87e63471ecd1d";
+/******/ 	var hotCurrentHash = "9ce6beeac5b3c00ad0f2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
