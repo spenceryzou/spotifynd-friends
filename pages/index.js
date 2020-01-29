f8b6e0ed604e0528a78e22aa5860d5e97828449a
Evelyn Yang <changminyang@169-231-147-221.wireless.ucsb.edu>
2020-01-29 15:05:54 -0800

    ey - added link as reference

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 //https://medium.com/swlh/how-to-host-your-next-js-application-for-free-on-heroku-c6ba99ebb8b8
+//https://medium.com/@jacoboakley/deploy-a-next-js-app-on-heroku-69bcb01db1b7
 const Index = () => (
     <div>
       <p>Hello World!</p>
