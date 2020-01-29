ad1cd23696af8e6536839ec6284ff4afa05a5de6
Evelyn Yang <changminyang@169-231-147-221.wireless.ucsb.edu>
2020-01-29 14:58:58 -0800

    ey changed scripts

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,11 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-<<<<<<< HEAD
-/******/ 	var hotCurrentHash = "0b891957af0905867853";
-=======
-/******/ 	var hotCurrentHash = "94b1d05d7edc9ea9aac8";
->>>>>>> 42878f6da7a166466a4397f9f595b46460f7d2aa
+/******/ 	var hotCurrentHash = "4c3dca9152d73c51f995";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
