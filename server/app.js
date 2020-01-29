d9684d9f7d51557a3714eb3e3a7586fbeac86557
Spencer Zou <spencerzou@ucsb.edu>
2020-01-29 15:49:55 -0800

    used app set port

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-var port = process.env.port || 3000;
+
 
 app.use('/', express.static('./dist', {index: "index.html"}))
 app.get('/callback/', express.static('./dist', {index: "index.html"}))
-
+app.set('port', process.env.PORT || 5000);
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
