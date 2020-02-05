a5fe2c54f9f025a7c49831c42f8b0d1292b672ba
Evelyn Yang <changminyang@169-231-144-227.wireless.ucsb.edu>
2020-02-04 20:44:12 -0800

    ey - changed dev script

--- pages/index.js
+++ pages/index.js
@@ -19,13 +19,13 @@ class Spotify extends Component {
     }
 
     
-    componentDidMount = () => {
-        let url = window.location.href
-        if(url.indexOf('_token')>-1){            
-            let access_token = url.split('_token=')[1].split("&")[0].trim()
-            this.setState({ access_token })
-        }
-    }
+    // componentDidMount = () => {
+    //     let url = window.location.href
+    //     if(url.indexOf('_token')>-1){            
+    //         let access_token = url.split('_token=')[1].split("&")[0].trim()
+    //         this.setState({ access_token })
+    //     }
+    // }
 
     generateRandomString = (length) => {
       var text = '';
@@ -63,7 +63,7 @@ class Spotify extends Component {
                 
                 <div className="row justify-content-center mt-5">
                     <button onClick={event => this.makeSpotifyProfileCall(event)} className="btn btn-success">
-                        { access_token !== '' ? 'Proceed to spotifyNext' : 'Login' }
+                        { access_token !== '' ? 'Click to enter Spotifynd' : 'Login' }
                     </button>
                 </div>
         );
