4d1221bc3e0cf67e04f29970c3848781a7a34c79
Lan-Chi Ho <aileemint17@Lan-Chis-MacBook-Pro.local>
2020-02-05 00:24:04 -0800

    lh fixed user.js maybe

--- pages/user.js
+++ pages/user.js
@@ -4,7 +4,7 @@ var express = require('express'); // Express web server framework
 var request = require('request'); // "Request" library
 
 
- export default (req, res) => {
+export default (req, res) => {
    var code = req.query.code || null;
    var state = req.query.state || null;
    var storedState = req.cookies ? req.cookies[stateKey] : null;
@@ -13,19 +13,20 @@ var request = require('request'); // "Request" library
      res.redirect('/#' +
      querystring.stringify({
       error: 'state_mismatch'
-    }));
-  } else {
+      }));
+    } else {
     res.clearCookie(stateKey);
+
     var authOptions = {
       url: 'https://accounts.spotify.com/api/token',
       form: {
         code: code,
         redirect_uri: redirect_uri,
         grant_type: 'authorization_code'
-    },
+      },
     headers: {
       'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
     },
     json: true
+
   };
- }
