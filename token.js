615fc567aa989491745eb47d7b4f8774e4913845
Lan-Chi Ho <aileemint17@169-231-167-1.wireless.ucsb.edu>
2020-02-05 16:49:29 -0800

    lh- trying to log body.text()

--- token.js
+++ token.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import Router from 'next/router'
 
 import querystring from "querystring";
-
+var client_id = '2923d79235804ea58633989710346f3d'; // Your client id
+var client_secret = 'd4813d196edf4940b58ba0aeedbf9ebc'; // Your secret
 const url = 'https://accounts.spotify.com/api/token';
 
 
@@ -14,15 +15,19 @@ export function getAccessToken(code){
     code: code,
     redirect_uri: 'https://spotifynd-friends.herokuapp.com/',
     grant_type: 'authorization_code',
+
     headers: {
-      'Content-Type': 'application/json'
+       'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+     },
       // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
+
+    //body: JSON.stringify(data)
   };
-    let resJson = fetch(url, fetchData)
+    fetch(url, fetchData)
       .then(response => response.json())
+      .then(console.log(body.text()))
       .then(json => json);
 
-    console.log(resJson);
-    return resJson;
+
+
 };
