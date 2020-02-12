7736896884c05e9e43d79fcc5df2f00f89bb4a6b
dariansung <dariansung@umail.ucsb.edu>
2020-02-12 14:22:23 -0800

    ds - refactored <Spotify /> into const comp for renaming/test writing convenience

--- test/test.js
+++ test/test.js
@@ -10,12 +10,13 @@ import Spotify from '../pages/index.js';
 configure({ adapter: new Adapter() });
 
 describe('<Spotify />', () => {
+    const comp = <Spotify />;
     it('button displays text \'Login\' when user is not logged in', () => {
-        const wrapper = render(<Spotify />);
+        const wrapper = render(comp);
         expect(wrapper.text()).to.contain('Login');
     })
     it('button displays text \'Click to enter Spotifynd\' when user is logged in and access code is set', () => {
-        const wrapper = mount(<Spotify />);
+        const wrapper = mount(comp);
         wrapper.setState({access_token : "thisIsAFakeAccessToken"});
         expect(wrapper.render().text()).to.contain('Click to enter Spotifynd');
     })
