69f247205a47160507470e3a29a9fbee9634a60e
Spencer Zou <spencerzou@ucsb.edu>
2020-02-11 19:04:01 -0800

    sz - added test for set access token and configured mocha for mount full DOM

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
-import { configure, render } from 'enzyme';
+import { configure, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai';
 import { spy } from 'sinon';
+import 'jsdom-global/register';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Spotify from '../pages/index.js';
@@ -11,6 +12,11 @@ configure({ adapter: new Adapter() });
 describe('<Spotify />', () => {
     it('button displays text \'Login\' when user is not logged in', () => {
         const wrapper = render(<Spotify />);
-        expect(wrapper.text()).to.contain('TESTFAIL');
+        expect(wrapper.text()).to.contain('Login');
+    })
+    it('button displays text \'Click to enter Spotifynd\' when user is logged in and access code is set', () => {
+        const wrapper = mount(<Spotify />);
+        wrapper.setState({access_token : "thisIsAFakeAccessToken"});
+        expect(wrapper.render().text()).to.contain('Click to enter Spotifynd');
     })
 });
