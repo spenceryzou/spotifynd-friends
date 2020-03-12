f2cf696664418fec18cf97dc2a9b6985bc07b499
Ha Ha <eduardogalvez@umail.ucsb.edu>
2020-03-12 11:42:51 -0700

    eg - changed navbar font to circular-light.woff2

--- components/Header.js
+++ components/Header.js
@@ -12,21 +12,32 @@ const Header = (props) => {
   return(
     <div>
       <style jsx>{`
+  .navbar {
+    
+    font-family: circular-light.woff2;
+   
+}
+
+
+
           
       `}</style>
     <head>
+    
     <link
+    
     rel="stylesheet"
     href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
     crossOrigin="anonymous"
       />
       <link src="../fonts/circular-light.woff2" rel="stylesheet" />
+      
     </head>
-    <Navbar bg="light" expand="lg" className="navBar">
+    <Navbar bg="light" expand="lg" className="navBar" >
     <Navbar.Brand className="mr-auto">
         <Link href={`/user?access_token=${props.props}`} as='/user' passHref>
-          <Nav.Link className='navLink' style={{color: 'black'}}>Spotifynd Friends</Nav.Link>
+          <Nav.Link className='navLink' style={{color: 'black', font:'serif'}}>Spotifynd Friends</Nav.Link>
         </Link>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
